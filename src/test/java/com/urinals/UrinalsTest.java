b14ef1aefe3d791c8c7230c73817973a954cd505
josephtv <jthom175@asu.edu>
2022-10-27 11:08:59 -0700

    Initial commit

---
+++ src/test/java/com/urinals/UrinalsTest.java
@@ -0,0 +1,21 @@
+package com.urinals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+class UrinalsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void validString() {
+    }
+}
