358d4b923c93e03aadce45d3c756febb80a18549
josephtv <jthom175@asu.edu>
2022-10-27 13:41:00 -0700

    added function to read file

--- src/main/java/com/urinals/Urinals.java
+++ src/main/java/com/urinals/Urinals.java
@@ -1,11 +1,35 @@
 package com.urinals;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Urinals {
 
     static Urinals urinals = new Urinals();
-    public static void main(String[] args) {
+    ArrayList<String> inputs = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
         System.out.println("ICA8 Test: Urinals");
+        urinals.readFile();
         urinals.validString("abc-abc");
+    }
+
+    void readFile() throws IOException {
+        File file = new File("src/main/resources/urinal.dat");
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        String lineFromFile;
+
+        while ((lineFromFile = bufferedReader.readLine()) != null) {
+           if(lineFromFile.equals("-1")) {
+               break;
+           }
+           inputs.add(lineFromFile);
+        }
+
+        System.out.println(inputs.toString());
+
     }
 
     Boolean validString(String str) {
