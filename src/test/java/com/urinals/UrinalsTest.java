7bc6ee7a2555e92fc501e9ef6d065176847add69
josephtv <jthom175@asu.edu>
2022-10-27 18:18:34 -0700

    Added test case for file read and write

--- src/test/java/com/urinals/UrinalsTest.java
+++ src/test/java/com/urinals/UrinalsTest.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
+import java.io.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author JOSEPH THOMAS
@@ -25,7 +24,7 @@ class UrinalsTest {
 
     @Test
     public void validString_Test1() {
-
+        //checking valid string
         System.out.println("====== Joseph Thomas == TEST ONE EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "101010";
@@ -34,7 +33,7 @@ class UrinalsTest {
 
     @Test
     public void validString_Test2() {
-
+        //checking for invalid string
         System.out.println("====== Joseph Thomas == TEST TWO EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "abc-abc";
@@ -43,7 +42,7 @@ class UrinalsTest {
 
     @Test
     public void validString_Test3() {
-
+        //checking for string length greater than 20
         System.out.println("====== Joseph Thomas == TEST THREE EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "111111111100000000001";
@@ -53,7 +52,7 @@ class UrinalsTest {
 
     @Test
     public void validString_Test4() {
-
+        //checking string length less than 1
         System.out.println("====== Joseph Thomas == TEST FOUR EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "";
@@ -62,6 +61,7 @@ class UrinalsTest {
 
     @Test
     void countUrinals_Test1() {
+        //checking countUrinals function give expected outcome
         System.out.println("====== Joseph Thomas == TEST FIVE EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "10001";
@@ -70,6 +70,7 @@ class UrinalsTest {
 
     @Test
     void countUrinals_Test2() {
+        //checking countUrinals function give expected outcome
         System.out.println("====== Joseph Thomas == TEST SIX EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "1001";
@@ -78,6 +79,7 @@ class UrinalsTest {
 
     @Test
     void countUrinals_Test3() {
+        //checking countUrinals function give expected outcome
         System.out.println("====== Joseph Thomas == TEST SEVEN EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "00000";
@@ -86,6 +88,7 @@ class UrinalsTest {
 
     @Test
     void countUrinals_Test4() {
+        //checking countUrinals function give expected outcome
         System.out.println("====== Joseph Thomas == TEST EIGHT EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "0000";
@@ -94,6 +97,7 @@ class UrinalsTest {
 
     @Test
     void countUrinals_Test5() {
+        //checking countUrinals function give expected outcome
         System.out.println("====== Joseph Thomas == TEST NINE EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "01000";
@@ -102,6 +106,7 @@ class UrinalsTest {
 
     @Test
     void countUrinals_Test6() {
+        //checking countUrinals function give expected outcome
         System.out.println("====== Joseph Thomas == TEST TEN EXECUTED =======");
         Urinals urinals = new Urinals();
         String testString = "011";
@@ -109,9 +114,33 @@ class UrinalsTest {
     }
 
     @Test
-    public void testReadFile() {
+    public void testReadFile_Test1() {
+        //checking file exists
         System.out.println("====== Joseph Thomas == TEST ELEVEN EXECUTED =======");
         File file = new File("src/main/resources/urinal.dat");
         assertTrue(file.exists());
     }
+
+    @Test
+    public void testReadFile_Test2() throws IOException {
+        //checking for empty file
+        System.out.println("====== Joseph Thomas == TEST TWELVE EXECUTED =======");
+        File file = new File("src/main/resources/urinal.dat");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/urinal.dat"));
+        if (bufferedReader.readLine() == null) {
+            System.out.println("No errors, and file empty");
+        }
+        assertTrue(file.exists());
+        assertNotNull(bufferedReader.readLine());
+    }
+
+    @Test
+    public void testWrite() {
+        //checks valid output file name
+        System.out.println("====== Joseph Thomas == TEST THIRTEEN EXECUTED =======");
+        File file = new File("src/main/resources/rule7.txt");
+        String name = file.getName();
+        assertTrue(name.contains("rule"));
+    }
+
 }
